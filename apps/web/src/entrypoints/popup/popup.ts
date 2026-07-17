7bb3261399cb7f10d8a80695d63b0279b301bed5
dragon <975425198@qq.com>
2026-07-17 15:19:19 +0800

    tmp

--- apps/web/src/entrypoints/popup/popup.ts
+++ apps/web/src/entrypoints/popup/popup.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import { detectInitialLocale } from '@/i18n/detect'
 import { setAppI18n, setupI18n } from '@/i18n/index'
 import App from './App.vue'
 
@@ -7,7 +6,7 @@ import '@/assets/index.css'
 import '@/assets/less/theme.less'
 
 async function main() {
-  const i18n = await setupI18n(detectInitialLocale())
+  const i18n = await setupI18n()
   setAppI18n(i18n)
   createApp(App).use(i18n).mount(`#app`)
 }
